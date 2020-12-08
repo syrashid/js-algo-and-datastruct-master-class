f35aea437f06bd8b6121cb7008a5b9b74fa05674
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-08 15:29:56 +0100

    FEAT: Implemented performance tests on the two add functions

--- section-two/add_up_to_n.js
+++ section-two/add_up_to_n.js
@@ -1,3 +1,5 @@
+const performance = require('perf_hooks').performance;
+
 const addUpTo = (n) => {
   let total = 0;
   for(i=0; i<=n; i++) {
@@ -11,5 +13,15 @@ const simpleAddUpTo = (n) => {
   return n * (n + 1) / 2
 }
 
-console.log(simpleAddUpTo(5));
-console.log(addUpTo(5));
+// console.log(simpleAddUpTo(5));
+// console.log(addUpTo(5));
+
+const t0 = performance.now();
+addUpTo(1000000000);
+const t1 = performance.now();
+console.log(`Time elapsed for complex add function is ${t1 - t0} milliseconds`);
+
+const p0 = performance.now();
+simpleAddUpTo(1000000000);
+const p1 = performance.now();
+console.log(`Time elapsed for simple add function is ${p1 - p0} milliseconds`);
