1ffe77a6707e6e89a02fcd02a2e168a551ecbf1d
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-12 18:10:54 +0100

    FEAT: Wrote test cases

---
+++ section-six/same_frequency.js
@@ -0,0 +1,4 @@
+console.log(`Test Passed: ${sameFrequency(182, 281) === true}`);
+console.log(`Test Passed: ${sameFrequency(34, 14) === false}`);
+console.log(`Test Passed: ${sameFrequency(3589578, 5879385) === true}`);
+console.log(`Test Passed: ${sameFrequency(22, 222) === false}`);
