ea1a5b759515d9caf917e9c5078921f55ea650d8
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-12 19:10:31 +0100

    FEAT: Implemented tests

---
+++ section-six/is_subsequence.js
@@ -0,0 +1,8 @@
+console.log(`Test Passed: ${isSubsequence('hello', 'hello world') === true}`);
+console.log(`Test Passed: ${isSubsequence('sing', 'sting') === true}`);
+console.log(`Test Passed: ${isSubsequence('abc', 'abracadabra') === true}`);
+console.log(`Test Passed: ${isSubsequence('abc', 'acb') === false}`);
+
+function isSubsequence() {
+
+}
