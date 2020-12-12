573095a350e6beeaa498b994de1c7f8b99861538
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-12 19:14:29 +0100

    FEAT: Psuedocode

--- section-six/is_subsequence.js
+++ section-six/is_subsequence.js
@@ -3,6 +3,12 @@ console.log(`Test Passed: ${isSubsequence('sing', 'sting') === true}`);
 console.log(`Test Passed: ${isSubsequence('abc', 'abracadabra') === true}`);
 console.log(`Test Passed: ${isSubsequence('abc', 'acb') === false}`);
 
-function isSubsequence() {
-
+function isSubsequence(sub, full) {
+  // set up pointer on sub string
+  let pointer = 0;
+  // iterate over chars of full string
+    // as characters match, move pointer up
+    // if characters don't match, keep iterating
+  // if pointer is greater than sub string length return false
+  // if you can iterate all the way thru and pointer reaches end return true
 }
