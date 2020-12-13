bff537336aefac4ee03bdf2e7a77380eefa9950f
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-13 16:56:58 +0100

    FEAT: Added tests for longest substring

---
+++ section-six/find_longest_substring.js
@@ -0,0 +1,7 @@
+findLongestSubstring('') // 0
+findLongestSubstring('rithmschool') // 7
+findLongestSubstring('thisisawesome') // 6
+findLongestSubstring('thecatinthehat') // 7
+findLongestSubstring('bbbbbb') // 1
+findLongestSubstring('longestsubstring') // 8
+findLongestSubstring('thisishowwedoit') // 6
