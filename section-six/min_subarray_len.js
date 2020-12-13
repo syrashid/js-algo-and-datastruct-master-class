dea09574e894bddbb7a5ec8700f117b9ae798275
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-13 15:57:58 +0100

    FEAT: Wrote psuedocode

--- section-six/min_subarray_len.js
+++ section-six/min_subarray_len.js
@@ -5,3 +5,13 @@ console.log(3, minSubArrayLen([1,4,16,22,5,7,8,9,10],39)) // 3
 console.log(5, minSubArrayLen([1,4,16,22,5,7,8,9,10],55)) // 5
 console.log(2, minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)) // 2
 console.log(0, minSubArrayLen([1,4,16,22,5,7,8,9,10],95)) // 0
+
+function minSubArrayLen(arr, target) {
+  // sum all elements in the array
+  // return 0 if unable to meet target
+  // create left and right pointers
+  // while left and right dont cross over, whittle window down
+    // check if taking off left puts you under, react accordingly
+    // check if taking off right puts you under, react accordingly
+  // return left - right after  you break out of while loop
+}
