bcf2ac5fb776a1b9b23cfc6c574e2e6fd5628a31
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-13 17:01:42 +0100

    FIX: Minor css fix to plant_tags

--- section-six/find_longest_substring.js
+++ section-six/find_longest_substring.js
@@ -5,3 +5,17 @@ console.log('7: ',findLongestSubstring('thecatinthehat')); // 7
 console.log('1: ',findLongestSubstring('bbbbbb')); // 1
 console.log('8: ',findLongestSubstring('longestsubstring')); // 8
 console.log('6: ',findLongestSubstring('thisishowwedoit')); // 6
+
+function findLongestSubstring(str) {
+  // Break array into chars and build frequency map
+  const freq = {}
+
+  for(let char of str.split('')) {
+    freq[char] = (freq[char] || 0) +  1;
+  }
+  console.log(freq);
+  // Set left and right pointers
+  // While window can be shifted
+    // reduce window based on pointer value and frequency map
+  // return diff between left and right pointer
+}
