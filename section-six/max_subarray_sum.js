f4c58a5253e0b064c14f30a03b9e954b8b3474e4
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-13 15:40:49 +0100

    FEAT: Passed tests

--- section-six/max_subarray_sum.js
+++ section-six/max_subarray_sum.js
@@ -1,6 +1,6 @@
 console.log(`Test Passed: ${maxSubarraySum([100,200,300,400], 2) === 700}`);
 console.log(`Test Passed: ${maxSubarraySum([1,4,2,10,23,3,1,0,20], 4) === 39}`);
-console.log(`Test Passed: ${maxSubarraySum([-3,4,0,-2,6,1], 2) === 5}`);
+console.log(`Test Passed: ${maxSubarraySum([-3,4,0,-2,6,-1], 2) === 5}`);
 console.log(`Test Passed: ${maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1], 2) === 5}`);
 console.log(`Test Passed: ${maxSubarraySum([2,3], 3) === null}`);
 
@@ -14,6 +14,14 @@ function maxSubarraySum(arr, target) {
     max += ele;
   }
   // Iterate over the array to augment the max
-  console.log(max);
+  let tempMax = max;
+  for(let i = 0; i < arr.length - target; i++) {
+    tempMax = tempMax + arr[i + target] - arr[i];
+    console.log('index:', i);
+    console.log("max:", max);
+    console.log('tempmax:', tempMax);
+    console.log('');
+    if (tempMax > max) max = tempMax;
+  }
   return max;
 }
