56a26d4741b0580d99bd6fff4eb7fa01fbdc8f70
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-13 15:51:30 +0100

    FEAT: Wrote test cases for min subarray len

---
+++ section-six/min_subarray_len.js
@@ -0,0 +1,7 @@
+console.log(2, minSubArrayLen([2,3,1,2,4,3], 7)) // 2 - because [4,3] is the smallestsubarray
+console.log(2, minSubArrayLen([2,1,6,5,4], 9)) // 2 - because [5,4] is the smallestsubarray
+console.log(1, minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52)) // 1 - because [62] is greater than 52
+console.log(3, minSubArrayLen([1,4,16,22,5,7,8,9,10],39)) // 3
+console.log(5, minSubArrayLen([1,4,16,22,5,7,8,9,10],55)) // 5
+console.log(2, minSubArrayLen([4, 3, 3, 8, 1, 2, 3], 11)) // 2
+console.log(0, minSubArrayLen([1,4,16,22,5,7,8,9,10],95)) // 0
