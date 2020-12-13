83194f26251ab81dbf7f9c2dd7b9bda1d963237f
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-13 11:17:39 +0100

    FEAT: Tests written for max subarray sum

---
+++ section-six/max_subarray_sum.js
@@ -0,0 +1,9 @@
+console.log(`Test Passed: ${maxSubarraySum([100,200,300,400], 2) === 700}`);
+console.log(`Test Passed: ${maxSubarraySum([1,4,2,10,23,3,1,0,20], 4) === 39}`);
+console.log(`Test Passed: ${maxSubarraySum([-3,4,0,-2,6,1], 2) === 5}`);
+console.log(`Test Passed: ${maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1], 2) === 5}`);
+console.log(`Test Passed: ${maxSubarraySum([2,3], 3) === null}`);
+
+function maxSubarraySum(arr, target) {
+
+}
