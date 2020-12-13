a7f9d4d61e15f1c1461fc200916114ce478893ed
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-13 16:58:21 +0100

    FEAT: Added tests for longest substring

--- section-six/find_longest_substring.js
+++ section-six/find_longest_substring.js
@@ -1,7 +1,7 @@
-findLongestSubstring('') // 0
-findLongestSubstring('rithmschool') // 7
-findLongestSubstring('thisisawesome') // 6
-findLongestSubstring('thecatinthehat') // 7
-findLongestSubstring('bbbbbb') // 1
-findLongestSubstring('longestsubstring') // 8
-findLongestSubstring('thisishowwedoit') // 6
+console.log('0: ',findLongestSubstring('')); // 0
+console.log('7: ',findLongestSubstring('rithmschool')); // 7
+console.log('6: ',findLongestSubstring('thisisawesome')); // 6
+console.log('7: ',findLongestSubstring('thecatinthehat')); // 7
+console.log('1: ',findLongestSubstring('bbbbbb')); // 1
+console.log('8: ',findLongestSubstring('longestsubstring')); // 8
+console.log('6: ',findLongestSubstring('thisishowwedoit')); // 6
