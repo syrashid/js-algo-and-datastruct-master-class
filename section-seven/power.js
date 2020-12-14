13edbf51788d57caabb2a3a9ddc46fc64af550af
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-14 13:50:48 +0100

    FEAT: Solved for power exercise

--- section-seven/power.js
+++ section-seven/power.js
@@ -2,6 +2,7 @@ console.log('1: ',power(2,0)); // 1
 console.log('4: ',power(2,2)); // 4
 console.log('16: ',power(2,4)); // 16
 
-function power(base,powe) {
-
+function power(base,pow) {
+  if (pow === 0) return 1;
+  return base * power(base, pow - 1);
 }
