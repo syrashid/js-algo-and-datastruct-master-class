ca3e4fed644b4aa988542448f7fb2070f9813c21
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-14 14:00:58 +0100

    FEAT: Added edge case guard clause for product of array

--- section-seven/product_of_array.js
+++ section-seven/product_of_array.js
@@ -1,7 +1,9 @@
+console.log('null: ', productOfArray([])); // null
 console.log('6: ', productOfArray([1,2,3])); // 6
 console.log('60: ', productOfArray([1,2,3,10])); // 60
 
 function productOfArray(arr) {
+  if (arr.length === 0) return null;
   if (arr.length === 1) return arr[0];
   return arr[0] * productOfArray(arr.slice(1));
 }
