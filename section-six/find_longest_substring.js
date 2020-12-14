aac42d655d83ca0d795e3b39a4f4472484129424
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-14 10:20:26 +0100

    FEAT: Added additional comments

--- section-six/find_longest_substring.js
+++ section-six/find_longest_substring.js
@@ -1,10 +1,10 @@
 console.log('0: ',findLongestSubstring('')); // 0
-console.log('7: ',findLongestSubstring('rithmschool')); // 7
-console.log('6: ',findLongestSubstring('thisisawesome')); // 6
-console.log('7: ',findLongestSubstring('thecatinthehat')); // 7
-console.log('1: ',findLongestSubstring('bbbbbb')); // 1
-console.log('8: ',findLongestSubstring('longestsubstring')); // 8
-console.log('6: ',findLongestSubstring('thisishowwedoit')); // 6
+console.log('7: ',findLongestSubstring('rithmschool')); // 7 rithmsc
+console.log('6: ',findLongestSubstring('thisisawesome')); // 6 awesom
+console.log('7: ',findLongestSubstring('thecatinthehat')); // 7 hecatin
+console.log('1: ',findLongestSubstring('bbbbbb')); // 1 b
+console.log('8: ',findLongestSubstring('longestsubstring')); // 8 ubstring
+console.log('6: ',findLongestSubstring('thisishowwedoit')); // 6 wedoit
 
 function findLongestSubstring(str) {
   // Break array into chars and build frequency map
@@ -18,6 +18,7 @@ function findLongestSubstring(str) {
   while (shifted) {
     // reduce window based on pointer value and frequency map
     shifted = false;
+
   }
 
   // return diff between left and right pointer
@@ -33,3 +34,8 @@ function buildFreqMap(arr) {
 
   return freq;
 }
+
+
+// Window starts with whole string
+// Use set and check if it's equal to string, if not recurse one down going one off the left pointer and one off the right, return when you get the matching one
+// Feels like the complexity of that is greater than O(n)
