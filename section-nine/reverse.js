6229313a21917b17065e1611c70bd78fba0e3f6f
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-15 06:33:36 +0100

    FEAT: Set up tests for reverse case

---
+++ section-nine/reverse.js
@@ -0,0 +1,6 @@
+function reverse(){
+  // add whatever parameters you deem necessary - good luck!
+}
+
+console.log('emosewa: ', reverse('awesome')) // 'emosewa'
+console.log('loohcsmhtir: ', reverse('rithmschool')) // 'loohcsmhtir'
