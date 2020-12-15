3ed7cd0085b4f6b9607e7f97a0bb45e05013ece2
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-15 11:17:12 +0100

    FEAT: Implmented binary search

--- section-ten/binary_search.js
+++ section-ten/binary_search.js
@@ -10,8 +10,15 @@ function binarySearch(arr, target) {
   let left = 0;
   let right = arr.length - 1;
 
-  // while (left < right) {
-
-  // }
+  while (left <= right) {
+    let middle = Math.ceil((right + left)/2);
+    if (arr[middle] === target) {
+      return middle;
+    } else if (arr[middle] > target) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
   return -1;
 }
