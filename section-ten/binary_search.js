0824dac6da38ecf2a7935e55ea4bb3da2d59b8d1
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-15 11:01:23 +0100

    FEAT: Built tests for binary search

--- section-ten/binary_search.js
+++ section-ten/binary_search.js
@@ -1,11 +1,17 @@
-
+console.log('1: ', binarySearch([1,2,3,4,5],2)); // 1
+console.log('2: ', binarySearch([1,2,3,4,5],3)); // 2
+console.log('4: ', binarySearch([1,2,3,4,5],5)); // 4
+console.log('-1: ', binarySearch([1,2,3,4,5],6)); // -1
+console.log('2: ', binarySearch([ 5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 10)); // 2
+console.log('16: ', binarySearch([ 5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 95)); // 16
+console.log('-1: ', binarySearch([ 5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99], 100)); // -1
 
 function binarySearch(arr, target) {
   let left = 0;
   let right = arr.length - 1;
 
-  while (left < right) {
+  // while (left < right) {
 
-  }
+  // }
   return -1;
 }
