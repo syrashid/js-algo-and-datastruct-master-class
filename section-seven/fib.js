e09973a95fea6130a3322ce9e9d83251ed31f689
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-15 06:19:56 +0100

    FIX: Fib refactor

--- section-seven/fib.js
+++ section-seven/fib.js
@@ -4,7 +4,6 @@ console.log('317811: ', fib(28)); // 317811
 console.log('9227465: ', fib(35)); // 9227465
 
 function fib(num){
-  if (num == 1) return 1;
-  if (num == 2) return 1;
+  if (num <= 2) return 1;
   return fib(num - 1) + fib(num - 2)
 }
