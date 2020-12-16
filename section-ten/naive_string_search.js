13940d8fbe8a6e3d365ca4a7b0c79e75ae392e46
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-16 15:12:16 +0100

    FEAT: Implemented naive string search

--- section-ten/naive_string_search.js
+++ section-ten/naive_string_search.js
@@ -1,5 +1,15 @@
 function naiveStringSearch(str, subStr) {
+  strChars = str.split('');
+  subStrChars = subStr.split('');
+  count = 0;
 
+  for(let i = 0; i < strChars.length; i++) {
+    for(let j = 0; j < subStrChars.length; j++) {
+      if (strChars[i + j] !== subStrChars[j]) break;
+      if (j === subStrChars.length - 1) count++;
+    }
+  }
+  return count;
 }
 
-console.log("Test Passed: ", naiveStringSearch("wowomgzomgwow", "omg") === 2)
+console.log("Test Passed: ", naiveStringSearch("wowomgzomgwomgw", "omg") === 3)
