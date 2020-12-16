c1ba57ed2cbab7ac15254f1f164fc6d193ee5296
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-16 12:52:54 +0100

    TEST: Set up test case for capitalize words

---
+++ section-nine/capitalize_words.js
@@ -0,0 +1,6 @@
+function capitalizeWords () {
+  // add whatever parameters you deem necessary - good luck!
+}
+
+let words = ['i', 'am', 'learning', 'recursion'];
+console.log(capitalizedWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
