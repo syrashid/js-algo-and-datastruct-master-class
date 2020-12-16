6b029c3bc75ae389545a79af45e3e5d4921daf13
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-16 13:12:11 +0100

    TEST: Wrote test for collect strings function

---
+++ section-nine/collect_strings.js
@@ -0,0 +1,22 @@
+function collectStrings(obj) {
+
+}
+
+
+const testObj = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+
+console.log(collectStrings(testObj)); // ["foo", "bar", "baz"])
