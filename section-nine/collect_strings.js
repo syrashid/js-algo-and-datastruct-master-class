13af1f8c269cb6abe9d2d31596de10f758404841
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-16 13:16:57 +0100

    FEAT: Solved for strings problem

--- section-nine/collect_strings.js
+++ section-nine/collect_strings.js
@@ -1,5 +1,18 @@
 function collectStrings(obj) {
+  // Establish strings array
+  let strings = [];
 
+  (function dig(nestObj) {
+    // Iterate through each part of the object
+    for (const key in nestObj) {
+      if (typeof nestObj[key] === 'string') strings.push(nestObj[key]);
+      // Dig as necessary
+      if (nestObj[key] instanceof Object) dig(nestObj[key]);
+    }
+    return;
+  })(obj);
+
+  return strings;
 }
 
 
