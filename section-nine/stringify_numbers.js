ee574d44ed8dfcd8ee52480a4ccefc0b30e90e99
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-16 13:03:55 +0100

    TEST: Wrote test case for stringify numbers

---
+++ section-nine/stringify_numbers.js
@@ -0,0 +1,33 @@
+function stringifyNumbers(obj) {
+
+}
+
+
+let testObj = {
+    num: 1,
+    test: [],
+    data: {
+        val: 4,
+        info: {
+            isRight: true,
+            random: 66
+        }
+    }
+}
+
+
+console.log(stringifyNumbers(testObj));
+
+/*
+{
+    num: "1",
+    test: [],
+    data: {
+        val: "4",
+        info: {
+            isRight: true,
+            random: "66"
+        }
+    }
+}
+*/
