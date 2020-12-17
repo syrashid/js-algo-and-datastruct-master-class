9c94ecb0c3ead31b9042f03a783a764b90bd998b
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-17 10:08:51 +0100

    FEAT: Added in a flag to optimize bubble sort

--- section-eleven/bubble_sort.js
+++ section-eleven/bubble_sort.js
@@ -2,10 +2,16 @@ console.log(bubbleSort([5, 1, 4, 2, 8])) // [1, 2, 4, 5, 8]
 console.log(swap([1,2], 0, 1)) // [2, 1]
 
 function bubbleSort(arr) {
+  let noSwaps;
   for(let i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for(let j = 0; j < i - 1; j++) {
-      if (arr[j] > arr[j+1]) swap(arr, j, j+1);
+      if (arr[j] > arr[j+1]) {
+        swap(arr, j, j+1);
+        noSwaps = false;
+      }
     }
+    if (noSwaps) break;
   }
   return arr;
 }
