0600abafd889e8cf18a01d21b6e0b90c91e6445c
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-17 09:57:03 +0100

    FEAT: Implemented swap method

--- section-eleven/bubble_sort.js
+++ section-eleven/bubble_sort.js
@@ -2,7 +2,12 @@ console.log(bubbleSort([5, 1, 4, 2, 8])) // [1, 2, 4, 5, 8]
 console.log(swap([1,2], 0, 1)) // [2, 1]
 
 function bubbleSort(arr) {
-
+  for(let i = array.length; i > 0; i--) {
+    for(let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j+1]) swap(arr, j, j+1)
+    }
+  }
+  return arr;
 }
 
 function swap(arr, pos1, pos2) {
