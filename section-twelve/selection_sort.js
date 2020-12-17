46af09642f1201627da21357ab27ac49fc62e6a8
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-17 10:32:17 +0100

    FEAT: Added in swap function again

--- section-twelve/selection_sort.js
+++ section-twelve/selection_sort.js
@@ -1,5 +1,10 @@
 console.log(selectionSort([5, 1, 4, 2, 8])) // [1, 2, 4, 5, 8]
+console.log(swap([1,2], 0, 1)) // [2, 1]
 
 function selectionSort(arr) {
 
+}
+
+function swap(arr, pos1, pos2) {
+  return [arr[pos1], arr[pos2]] = [arr[pos2], arr[pos1]]
 }
