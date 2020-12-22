682dd8592f0ae6e9a544ab072657710d35960a03
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-22 12:52:53 +0100

    TEST: most Digits test case

--- section-seventeen/radix_sort.js
+++ section-seventeen/radix_sort.js
@@ -1,18 +1,20 @@
 console.log(radixSort([5, 1, 4, 2, 8, 10, 50, 9, 14, 99, 100]));
 console.log(radixSort([5, 1, 2]));
 
-console.log('5:', getDigit(12345, 0));
-console.log('4:', getDigit(12345, 1));
-console.log('3:', getDigit(12345, 2));
-console.log('2:', getDigit(12345, 3));
-console.log('1:', getDigit(12345, 4));
-console.log('0:', getDigit(12345, 5));
-console.log('0:', getDigit(12345, -1));
-console.log('0:', getDigit(12345, 100));
+console.log('5: ', getDigit(12345, 0));
+console.log('4: ', getDigit(12345, 1));
+console.log('3: ', getDigit(12345, 2));
+console.log('2: ', getDigit(12345, 3));
+console.log('1: ', getDigit(12345, 4));
+console.log('0: ', getDigit(12345, 5));
+console.log('0: ', getDigit(12345, -1));
+console.log('0: ', getDigit(12345, 100));
 
-console.log('1:', digitCount(1));
-console.log('2:', digitCount(10));
-console.log('3:', digitCount(100));
+console.log('1: ', digitCount(1));
+console.log('2: ', digitCount(10));
+console.log('3: ', digitCount(100));
+
+console.log('3: ',mostDigits([5, 1, 4, 2, 8, 10, 50, 9, 14, 99, 100]));
 
 function radixSort(arr) {
 
@@ -25,5 +27,9 @@ function getDigit(num,place) {
 }
 
 function digitCount(num) {
-  return parseInt(num).length
+  return num.toString().length
+}
+
+function mostDigits(arr) {
+
 }
