5e3faa6ef205729a8631119844f3b1fc1cb15e13
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-22 12:38:38 +0100

    TEST: Get digit tests and edge cases

--- section-seventeen/radix_sort.js
+++ section-seventeen/radix_sort.js
@@ -1,5 +1,6 @@
 console.log(radixSort([5, 1, 4, 2, 8, 10, 50, 9, 14, 99, 100]));
 console.log(radixSort([5, 1, 2]));
+
 console.log('5:', getDigit(12345, 0));
 console.log('4:', getDigit(12345, 1));
 console.log('3:', getDigit(12345, 2));
@@ -14,5 +15,5 @@ function radixSort(arr) {
 }
 
 function getDigit(num,place) {
-
+  return num.toString('')
 }
