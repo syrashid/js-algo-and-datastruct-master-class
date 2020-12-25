234647fbcb5b216b55fbb6e7fc9c4ea453215981
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-24 19:07:59 -0500

    FEAT: Set up base constructors for sll and node

---
+++ section-nineteen/singly_linked_list.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
