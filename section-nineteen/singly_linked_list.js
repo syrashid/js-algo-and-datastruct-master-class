4138aa0117b743dfdbee759f173aafe8abe17609
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-24 19:16:49 -0500

    FEAT: Implemented push method

--- section-nineteen/singly_linked_list.js
+++ section-nineteen/singly_linked_list.js
@@ -1,14 +1,26 @@
 class Node {
-  constructor(value) {
+  constructor(value){
     this.value = value;
     this.next = null;
   }
 }
 
 class SinglyLinkedList {
-  constructor() {
+  constructor(){
     this.head = null;
     this.tail = null;
     this.length = 0;
+  }
+
+  push(val){
+    const node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
   }
 }
