145677748ec95ad5d573cf08ae525eb86097be3b
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-25 13:23:51 -0500

    FEAT: Set up push method for dll

--- section-twenty/doubly_linked_list.js
+++ section-twenty/doubly_linked_list.js
@@ -13,4 +13,25 @@ class DoublyLinkedList {
     this.length = 0;
   }
 
+  push(val) {
+    const node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if(!this.tail) return undefined;
+    const oldTail = this.tail;
+    this.tail = oldTail.prev
+    this.tail.next = null;
+    this.length--;
+  }
 }
