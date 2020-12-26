d9c02c80dc6211197df50e7565031696a82c1ba9
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-26 09:17:55 -0500

    FEAT: Defined queue class with methods

---
+++ section-twentyone/queue.js
@@ -0,0 +1,41 @@
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(val){
+    const node = new Node(val);
+    if (!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      this.last.next = node;
+      this.last = node;
+    }
+    this.size++;
+    return this;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+    const popped = this.first;
+    this.first = popped.next;
+    popped.next = null;
+    this.size--;
+    if (this.size === 0) this.last = null;
+    return popped.value;
+  }
+
+
+
+}
