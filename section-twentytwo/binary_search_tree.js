3a43cec1a43ab386e0ceada1440c9738ecd0a371
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-27 09:10:16 -0500

    FEAT: Implement base bst insert

--- section-twentytwo/binary_search_tree.js
+++ section-twentytwo/binary_search_tree.js
@@ -12,6 +12,29 @@ class BinarySearchTree {
   }
 
   insert(val){
-
+    const node = new Node(val);
+    if(this.root) {
+      let comparison = this.root;
+      while(true){
+        if (comparison.value > node.value) {
+          if (comparison.left) {
+            comparison = comparison.left;
+          } else {
+            comparison.left = node;
+            break;
+          }
+        } else {
+          if (comparison.right) {
+            comparison = comparison.right;
+          } else {
+            comparison.right = node;
+            break;
+          }
+        }
+      }
+    } else{
+      this.root = node;
+    }
+    return this;
   }
 }
