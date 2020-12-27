55f79faf153c6d6d314f2b7bf86d7387a6890ee5
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-27 16:13:58 -0500

    ARCH: Added a binary search tree class to section twenty three

---
+++ section-twentythree/binary_search_tree.js
@@ -0,0 +1,55 @@
+class Node {
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor(){
+    this.root = null;
+  }
+
+  insert(val){
+    const node = new Node(val);
+    if(this.root) {
+      let comparison = this.root;
+      while(true){
+        if(node.value === comparison.value) return undefined;
+        if (comparison.value > node.value) {
+          if (comparison.left) {
+            comparison = comparison.left;
+          } else {
+            comparison.left = node;
+            break;
+          }
+        } else {
+          if (comparison.right) {
+            comparison = comparison.right;
+          } else {
+            comparison.right = node;
+            break;
+          }
+        }
+      }
+    } else{
+      this.root = node;
+    }
+    return this;
+  }
+
+  find(val){
+    let comparison = this.root;
+    while(comparison) {
+      if (val === comparison.value) {
+        return comparison;
+      } else if (val > comparison.value) {
+        comparison = comparison.right;
+      } else {
+        comparison = comparison.left;
+      }
+    }
+    return false;
+  }
+}
