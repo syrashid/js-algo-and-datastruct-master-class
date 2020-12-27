64d2c1ab2007f5561f0b5d6213a627f4367b48d3
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-27 16:16:06 -0500

    FEAT: Base architecture for tree example

--- section-twentythree/breadth_first_search.js
+++ section-twentythree/breadth_first_search.js
@@ -53,3 +53,14 @@ class BinarySearchTree {
     return false;
   }
 }
+
+
+const tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(20);
+tree.insert(3);
+tree.insert(8);
+
+console.log(tree);
