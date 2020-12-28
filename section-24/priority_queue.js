9c612ed2d71c2da264194aad61686c8b945a8ae1
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-28 10:13:17 -0500

    FEAT: Implemented priority queue

--- section-24/priority_queue.js
+++ section-24/priority_queue.js
@@ -18,10 +18,11 @@ class PriorityQueue {
 
   remove() {
     if (this.elements.length === 0) return undefined;
+    if (this.elements.length === 1) return this.elements.pop();
     this.swap(0, this.elements.length - 1);
-    let maxNode = this.elements.pop();
+    let minNode = this.elements.pop();
     this.trickle();
-    return maxNode;
+    return minNode;
   }
 
   // Private methods (not really)
@@ -32,36 +33,62 @@ class PriorityQueue {
 
   bubble() {
     let currentIdx = this.elements.length - 1;
-    let parentIdx = Math.floor((current - 1)/2);
+    let parentIdx = Math.floor((currentIdx - 1)/2);
     while(this.elements[parentIdx].priority > this.elements[currentIdx].priority) {
       this.swap(parentIdx, currentIdx);
       currentIdx = parentIdx;
-      parentIdx = Math.floor((currentIdx - 1)/2);
+      if (currentIdx > 0) parentIdx = Math.floor((currentIdx - 1)/2);
     }
   }
 
   trickle() {
     let currentIdx = 0;
-    let largestChildIdx = this.findLargestChildIdx(currentIdx);
+    let minChildIdx = this.findMinChildIdx(currentIdx);
 
-    while (this.elements[currentIdx].priority > this.elements[largestChildIdx].priority) {
-      this.swap(currentIdx, largestChildIdx);
-      currentIdx = largestChildIdx;
-      largestChildIdx = this.findLargestChildIdx(currentIdx);
+    while (this.elements[currentIdx].priority > this.elements[minChildIdx].priority) {
+      this.swap(currentIdx, minChildIdx);
+      currentIdx = minChildIdx;
+      minChildIdx = this.findMinChildIdx(currentIdx);
     }
   }
 
-  findLargestChildIdx(parentIdx) {
-    let leftChildPriority = null;
-    let rightChildPriority = null;
+  findMinChildIdx(parentIdx) {
+    let leftChildPriority = Infinity;
+    let rightChildPriority = Infinity;
     let leftChildIdx = 2 * parentIdx + 1;
     let rightChildIdx = 2 * parentIdx + 2;
 
     if (leftChildIdx < this.elements.length) leftChildPriority = this.elements[leftChildIdx].priority;
     if (rightChildIdx < this.elements.length) rightChildPriority = this.elements[rightChildIdx].priority;
 
-    return leftChildPriority > rightChildPriority ? leftChildIdx : rightChildIdx;
+    // If the parent has no children, return the parentIdx
+    if (leftChildPriority === Infinity && rightChildPriority === Infinity) return parentIdx;
+
+    return leftChildPriority < rightChildPriority ? leftChildIdx : rightChildIdx;
   }
+
+
 }
 
 
+const pq = new PriorityQueue();
+pq.insert('do homework', 10);
+pq.insert('go to gym', 20);
+pq.insert('random shit', 21);
+pq.insert('listen to music', 15);
+pq.insert('do finances', 3);
+pq.insert('eat something', 0);
+
+console.log(pq.elements);
+pq.remove();
+console.log(pq.elements);
+pq.remove();
+console.log(pq.elements);
+pq.remove();
+console.log(pq.elements);
+pq.remove();
+console.log(pq.elements);
+pq.remove();
+console.log(pq.elements);
+pq.remove();
+console.log(pq.elements);
