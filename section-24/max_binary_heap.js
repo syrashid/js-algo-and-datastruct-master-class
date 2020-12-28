40b8e8890be276be485cb6ff9536822c22ea10cd
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-28 09:14:38 -0500

    FEAT: Implemented remove function for binary heap

--- section-24/max_binary_heap.js
+++ section-24/max_binary_heap.js
@@ -8,6 +8,16 @@ class MaxBinaryHeap {
     if (this.values.length > 1) this.bubble();
   }
 
+  remove() {
+    if (this.values.length === 0) return undefined;
+    this.swap(0, this.values.length - 1);
+    let removed = this.values.pop();
+    this.trickle();
+    return removed;
+  }
+
+  // Private methods (not really)
+
   swap(pos1, pos2) {
     return [this.values[pos1], this.values[pos2]] = [this.values[pos2], this.values[pos1]]
   }
@@ -21,6 +31,29 @@ class MaxBinaryHeap {
       parent = Math.floor((current - 1)/2);
     }
   }
+
+  trickle() {
+    let currentIdx = 0;
+    let largestChildIdx = this.findLargestChildIdx(currentIdx);
+
+    while (this.values[currentIdx] < this.values[largestChildIdx]) {
+      this.swap(currentIdx, largestChildIdx);
+      currentIdx = largestChildIdx;
+      largestChildIdx = this.findLargestChildIdx(currentIdx);
+    }
+  }
+
+  findLargestChildIdx(parentIdx) {
+    let leftChild = null;
+    let rightChild = null;
+    let leftChildIdx = 2 * parentIdx + 1;
+    let rightChildIdx = 2 * parentIdx + 2;
+
+    if (leftChildIdx < this.values.length) leftChild = this.values[leftChildIdx];
+    if (rightChildIdx < this.values.length) rightChild = this.values[rightChildIdx];
+
+    return leftChild > rightChild ? leftChildIdx : rightChildIdx;
+  }
 }
 
 const mbh = new MaxBinaryHeap();
@@ -33,4 +66,13 @@ mbh.insert(100);
 console.log(mbh.values);
 mbh.insert(15);
 console.log(mbh.values);
-
+console.log(mbh.remove());
+console.log(mbh.values);
+console.log(mbh.remove());
+console.log(mbh.values);
+console.log(mbh.remove());
+console.log(mbh.values);
+console.log(mbh.remove());
+console.log(mbh.values);
+console.log(mbh.remove());
+console.log(mbh.values);
