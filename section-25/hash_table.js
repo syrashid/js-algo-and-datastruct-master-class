3158df715f85717080fa54488d89b90ac711248e
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-29 21:12:05 -0500

    ARCH: Set up base hash table class

---
+++ section-25/hash_table.js
@@ -0,0 +1,21 @@
+
+
+class HashTable {
+  constructor(size=53){
+    this.keyMap = Array.from(Array(size), () => []);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+}
+
+const hash_table = new HashTable();
+console.log(hash_table.keyMap);
