492e658a4f051448f52dfe4d8e0b7a7b71b11dfa
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-30 12:19:02 -0500

    FEAT: Wrote in addVertex method for graph class

--- section-26/graph.js
+++ section-26/graph.js
@@ -1,7 +1,20 @@
 class Graph {
-    constructor(){
+    constructor() {
         this.adjacencyList = new Map();
     }
 
+    addVertex(vertex) {
+      if (!this.adjacencyList.has(vertex)) this.adjacencyList.set(vertex, []);
+    }
 
+    printAdjList() {
+      console.log(this.adjacencyList);
+    }
 }
+
+const g = new Graph();
+g.addVertex('Tokyo');
+g.addVertex('Kyoto');
+g.printAdjList();
+g.addVertex('Kyoto');
+g.printAdjList();
