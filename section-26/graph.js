d5afb63410c5e20bcbfc2c7d98def268fa939f35
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-30 12:38:43 -0500

    FEAT: Added a remove edge method

--- section-26/graph.js
+++ section-26/graph.js
@@ -1,30 +1,43 @@
 class Graph {
-    constructor() {
-        this.adjacencyList = new Map();
-    }
+  constructor() {
+      this.adjacencyList = new Map();
+  }
 
-    addVertex(vtx) {
-      if (!this.adjacencyList.has(vtx)) this.adjacencyList.set(vtx, []);
-    }
+  addVertex(vtx) {
+    if (!this.adjacencyList.has(vtx)) this.adjacencyList.set(vtx, []);
+  }
 
-    addEdge(vtx1, vtx2) {
-      if (this._areValidVtx(vtx1,vtx2)) {
-        if (!this.adjacencyList.get(vtx1).includes(vtx2)) {
-          this.adjacencyList.get(vtx1).push(vtx2)
-        }
-        if (!this.adjacencyList.get(vtx2).includes(vtx1)) {
-          this.adjacencyList.get(vtx2).push(vtx1)
-        }
+  addEdge(vtx1, vtx2) {
+    if (this._areValidVtx(vtx1,vtx2)) {
+      if (!this.adjacencyList.get(vtx1).includes(vtx2)) {
+        this.adjacencyList.get(vtx1).push(vtx2);
+      }
+      if (!this.adjacencyList.get(vtx2).includes(vtx1)) {
+        this.adjacencyList.get(vtx2).push(vtx1);
       }
     }
+  }
 
-    printAdjList() {
-      console.log(this.adjacencyList);
+  removeEdge(vtx1, vtx2) {
+    if (this._areValidVtx(vtx1,vtx2)) {
+      if (this.adjacencyList.get(vtx1).includes(vtx2)) {
+        const vtx2Idx = this.adjacencyList.get(vtx1).indexOf(vtx2);
+        this.adjacencyList.get(vtx1).splice(vtx2Idx,1);
+      }
+      if (this.adjacencyList.get(vtx2).includes(vtx1)) {
+        const vtx1Idx = this.adjacencyList.get(vtx2).indexOf(vtx1);
+        this.adjacencyList.get(vtx2).splice(vtx1Idx,1);
+      }
     }
+  }
 
-    _areValidVtx(vtx1, vtx2) {
-      return this.adjacencyList.has(vtx1) && this.adjacencyList.has(vtx2)
-    }
+  printAdjList() {
+    console.log(this.adjacencyList);
+  }
+
+  _areValidVtx(vtx1, vtx2) {
+    return this.adjacencyList.has(vtx1) && this.adjacencyList.has(vtx2)
+  }
 }
 
 const g = new Graph();
@@ -34,5 +47,8 @@ g.addVertex('Osaka');
 g.addVertex('Kobe');
 g.printAdjList();
 g.addEdge('Tokyo', 'Kyoto');
-g.addEdge('Dallas', 'Tokyo');
+g.addEdge('Osaka', 'Tokyo');
+g.addEdge('Osaka', 'Kobe');
+g.printAdjList();
+g.removeEdge('Tokyo', 'Kyoto');
 g.printAdjList();
