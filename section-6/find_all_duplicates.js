c8c0da6991b76a0236b5242e0eae562060441cf2
Sy Rashid <sy.rashid.11@gmail.com>
2021-01-01 16:22:17 -0500

    TEST: Wrote test cases for find all duplicates

---
+++ section-6/find_all_duplicates.js
@@ -0,0 +1,7 @@
+console.log(findAllDuplicates([4,3,2,7,8,2,3,1])); // array with 2 and 3
+console.log(findAllDuplicates([4, 3, 2, 1, 0])); // []
+console.log(findAllDuplicates([4, 3, 2, 1, 0, 1, 2, 3])); // array with 3, 2, and 1
+
+function findAllDuplicates(arr) {
+
+}
