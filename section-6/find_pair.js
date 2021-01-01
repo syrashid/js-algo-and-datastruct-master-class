503e10b15a4d228db4c577649ad75a69f6e2e826
Sy Rashid <sy.rashid.11@gmail.com>
2021-01-01 16:12:03 -0500

    TEST: Wrote cases for findPair

---
+++ section-6/find_pair.js
@@ -0,0 +1,14 @@
+console.log('Passed: ', findPair([6,1,4,10,2,4], 2)); // true
+console.log('Passed: ', findPair([8,6,2,4,1,0,2,5,13],1)); // true
+console.log('Passed: ', findPair([4,-2,3,10],-6)); // true
+console.log('Passed: ', !findPair([6,1,4,10,2,4], 22)); // false
+console.log('Passed: ', !findPair([], 0)); // false
+console.log('Passed: ', findPair([5,5], 0)); // true
+console.log('Passed: ', findPair([-4,4], -8)); // true
+console.log('Passed: ', findPair([-4,4], 8)); // true
+console.log('Passed: ', findPair([1,3,4,6],-2)); // true
+console.log('Passed: ', findPair([0,1,3,4,6],-2)); // true
+
+function findPair(arr, target) {
+
+}
