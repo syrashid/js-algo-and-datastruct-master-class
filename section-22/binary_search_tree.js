2bed3a4cea6ca4d882592e8a2df553fe0df5dc41
Sy Rashid <sy.rashid.11@gmail.com>
2021-01-01 15:11:35 -0500

    FEAT: Solved removal for special case

--- section-22/binary_search_tree.js
+++ section-22/binary_search_tree.js
@@ -83,10 +83,10 @@ class BinarySearchTree {
     } else if (node.left !== null && node.right !== null) {
       let successor = node.right;
       while(successor.left) {
-        sucessor = successor.left;
+        successor = successor.left;
       }
-      parent.left === node ? parent.left = successor : parent.right = successor;
-      successor.left = node.left;
+      if (successor.right) this.remove(successor.value)
+      node.value = successor.value;
     } else {
       if (node.left) {
         parent.left === node ? parent.left = node.left : parent.right = node.left;
@@ -133,29 +133,45 @@ class BinarySearchTree {
 //   .insert(24)
 //   .insert(70);
 
-// console.log(binarySearchTree);
-// binarySearchTree.remove(10);
-// console.log(binarySearchTree.root.left.value); // 12
-// console.log(binarySearchTree.root.left.left.value); // 1
-// console.log(binarySearchTree.root.left.left.right.value); // 5
-// binarySearchTree.remove(50);
-// console.log(binarySearchTree.root.right.value); // 20
-// console.log(binarySearchTree.root.right.right.value); // 60
-// console.log(binarySearchTree.root.right.right.left.value); // 30
+console.log(binarySearchTree);
+binarySearchTree.remove(10);
+console.log(binarySearchTree.root.left.value); // 12
+console.log(binarySearchTree.root.left.left.value); // 1
+console.log(binarySearchTree.root.left.left.right.value); // 5
+binarySearchTree.remove(50);
+console.log(binarySearchTree.root.right.value); // 20
+console.log(binarySearchTree.root.right.right.value); // 60
+console.log(binarySearchTree.root.right.right.left.value); // 30
+
+// var binarySearchTree = new BinarySearchTree();
+// binarySearchTree
+//   .insert(22)
+//   .insert(49)
+//   .insert(85)
+//   .insert(66)
+//   .insert(95)
+//   .insert(90)
+//   .insert(100)
+//   .insert(88)
+//   .insert(93)
+//   .insert(89)
+
+// binarySearchTree.remove(85);
+// console.log(binarySearchTree.root.right.right.value); // 88
+// console.log(binarySearchTree.root.right.right.right.left.left.value); // 89
 
 var binarySearchTree = new BinarySearchTree();
 binarySearchTree
-  .insert(22)
-  .insert(49)
-  .insert(85)
-  .insert(66)
-  .insert(95)
-  .insert(90)
-  .insert(100)
-  .insert(88)
-  .insert(93)
-  .insert(89)
-
-binarySearchTree.remove(85);
-console.log(binarySearchTree.root.right.right.value); // 88
-console.log(binarySearchTree.root.right.right.right.left.left.value); // 89
+  .insert(15)
+  .insert(20)
+  .insert(10)
+  .insert(12)
+  .insert(1)
+  .insert(5)
+  .insert(50)
+  .insert(60)
+  .insert(30)
+  .insert(25)
+  .insert(23)
+  .insert(24)
+  .insert(70);
