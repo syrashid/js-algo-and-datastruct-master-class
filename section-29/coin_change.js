4a51f1ad046661c9989a8171cbfc6388c06f37cd
Sy Rashid <sy.rashid.11@gmail.com>
2021-01-01 16:56:35 -0500

    FEAT: Added coin change base cases

---
+++ section-29/coin_change.js
@@ -0,0 +1,16 @@
+const denominations = [1, 5, 10, 25]
+
+console.log('Combos: ', coinChange(denominations, 1)); // 1
+console.log('Combos: ', coinChange(denominations, 2)); // 1
+console.log('Combos: ', coinChange(denominations, 5)); // 2
+console.log('Combos: ', coinChange(denominations, 10)); // 4
+// console.log('Combos: ', coinChange(denominations, 25)); // 13
+// console.log('Combos: ', coinChange(denominations, 45)); // 39
+// console.log('Combos: ', coinChange(denominations, 100)); // 242
+// console.log('Combos: ', coinChange(denominations, 145)); // 622
+// console.log('Combos: ', coinChange(denominations, 1451)); // 425663
+// console.log('Combos: ', coinChange(denominations, 14511)); // 409222339
+
+function coinChange(denos, amount) {
+  if ()
+}
