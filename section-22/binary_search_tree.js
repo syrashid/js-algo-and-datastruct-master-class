c771a51e832420422186600d138f8479d1f56f06
Sy Rashid <sy.rashid.11@gmail.com>
2021-01-01 15:01:10 -0500

    TEST: Wrote out test case for root removal)

--- section-22/binary_search_tree.js
+++ section-22/binary_search_tree.js
@@ -117,30 +117,45 @@ class BinarySearchTree {
 // console.log(binarySearchTree.root.right.right); // null
 // console.log(binarySearchTree.root.right.left); // null
 
+// var binarySearchTree = new BinarySearchTree();
+// binarySearchTree
+//   .insert(15)
+//   .insert(20)
+//   .insert(10)
+//   .insert(12)
+//   .insert(1)
+//   .insert(5)
+//   .insert(50)
+//   .insert(60)
+//   .insert(30)
+//   .insert(25)
+//   .insert(23)
+//   .insert(24)
+//   .insert(70);
+
+// console.log(binarySearchTree);
+// binarySearchTree.remove(10);
+// console.log(binarySearchTree.root.left.value); // 12
+// console.log(binarySearchTree.root.left.left.value); // 1
+// console.log(binarySearchTree.root.left.left.right.value); // 5
+// binarySearchTree.remove(50);
+// console.log(binarySearchTree.root.right.value); // 20
+// console.log(binarySearchTree.root.right.right.value); // 60
+// console.log(binarySearchTree.root.right.right.left.value); // 30
+
 var binarySearchTree = new BinarySearchTree();
 binarySearchTree
-  .insert(15)
-  .insert(20)
-  .insert(10)
-  .insert(12)
-  .insert(1)
-  .insert(5)
-  .insert(50)
-  .insert(60)
-  .insert(30)
-  .insert(25)
-  .insert(23)
-  .insert(24)
-  .insert(70);
+  .insert(22)
+  .insert(49)
+  .insert(85)
+  .insert(66)
+  .insert(95)
+  .insert(90)
+  .insert(100)
+  .insert(88)
+  .insert(93)
+  .insert(89)
 
-console.log(binarySearchTree);
-binarySearchTree.remove(10);
-console.log(binarySearchTree.root.left.value); // 12
-console.log(binarySearchTree.root.left.left.value); // 1
-console.log(binarySearchTree.root.left.left.right.value); // 5
-binarySearchTree.remove(50);
-console.log(binarySearchTree.root.right.value); // 20
-console.log(binarySearchTree.root.right.right.value); // 60
-console.log(binarySearchTree.root.right.right.left.value); // 30
-
-// var binarySearchTree = new BinarySearchTree();binarySearchTree .insert(22) .insert(49) .insert(85) .insert(66) .insert(95) .insert(90) .insert(100) .insert(88) .insert(93) .insert(89)binarySearchTree.remove(85);binarySearchTree.root.right.right.value // 88binarySearchTree.root.right.right.right.left.left.value // 89
+binarySearchTree.remove(85);
+console.log(binarySearchTree.root.right.right.value); // 88
+console.log(binarySearchTree.root.right.right.right.left.left.value); // 89
