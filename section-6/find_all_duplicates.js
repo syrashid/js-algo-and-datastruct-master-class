539e44996bf9ec6ea481f1590d3d4fdf9b9066dd
Sy Rashid <sy.rashid.11@gmail.com>
2021-01-01 16:29:12 -0500

    FEAT: Implemented find all duplicates

--- section-6/find_all_duplicates.js
+++ section-6/find_all_duplicates.js
@@ -3,5 +3,13 @@ console.log(findAllDuplicates([4, 3, 2, 1, 0])); // []
 console.log(findAllDuplicates([4, 3, 2, 1, 0, 1, 2, 3])); // array with 3, 2, and 1
 
 function findAllDuplicates(arr) {
-
+  const counter = new Map();
+  const duplicates = [];
+  for(let element of arr) {
+    counter.has(element) ? counter.set(element, counter.get(element) + 1) : counter.set(element, 1);
+  }
+  counter.forEach((val, key) => {
+    if (val > 1) duplicates.push(key);
+  })
+  return duplicates;
 }
