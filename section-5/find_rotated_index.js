b238148ed4287aa60567b34835cc15f04fd5cf8a
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-31 21:09:42 -0500

    FEAT: Added in skeleton for findRotatedIndex with tests

---
+++ section-5/find_rotated_index.js
@@ -0,0 +1,10 @@
+console.log(1, findRotatedIndex([3,4,1,2],4)); // 1
+console.log(2, findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 8)); // 2
+console.log(6, findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 3)); // 6
+console.log(-1, findRotatedIndex([37,44,66,102,10,22],14));// -1
+console.log(-1, findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12));// -1
+console.log(5, findRotatedIndex([11,12,13,14,15,16,3,5,7,9], 16)); // 5
+
+function findRotatedIndex (arr, target) {
+
+}
--- section-5/sorted_frequency.js
+++ section-5/sorted_frequency.js
@@ -4,17 +4,22 @@ console.log(2, sortedFrequency([1,1,2,2,2,2,3],1)); // 2
 console.log(-1, sortedFrequency([1,1,2,2,2,2,3],4)); // -1
 
 function sortedFrequency(arr, target) {
-  console.log(findStart(arr, target), findEnd(arr, target));
+  const start = findStart(arr, target);
+  const end = findEnd(arr, target);
+  if (start === -1 && end === -1) return -1;
+  else if (start === -1) return arr.slice(0, end + 1).length;
+  else if (end === -1) return arr.slice(start).length;
+  else return end - start + 1;
+
 }
 
 function findStart(arr, target) {
   let left = 0;
   let right = arr.length - 1;
-
   while (left <= right) {
     const middle = Math.ceil((right + left)/2);
     if (arr[middle] === target && arr[middle - 1] < target) return middle;
-    (arr[middle] > target) ? right = middle - 1: left = middle + 1;
+    (arr[middle] >= target) ? right = middle - 1: left = middle + 1;
   }
   return -1;
 }
