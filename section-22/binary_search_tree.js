e7bd2e20129dcb059e7c392db78f50f0e0a036cf
Sy Rashid <sy.rashid.11@gmail.com>
2021-01-01 13:49:26 -0500

    FEAT: solved for remove with 1 child:

--- section-22/binary_search_tree.js
+++ section-22/binary_search_tree.js
@@ -74,30 +74,42 @@ class BinarySearchTree {
     let [node, parent] = this.findWithParent(val);
     // Case 1: Root Removal
     // Case 2: Leaf
-    // Case 3: Has 1 Child
-    // Case 4: Has 2 Children
+    // Case 3: Has 2 Children
+    // Case 4: Has 1 Child
     if(!parent) {
 
     } else if (node.left === null && node.right === null) {
       parent.left === node ? parent.left = null : parent.right = null;
-    } else if (node.left !== null || node.right !== null) {
+    } else if (node.left !== null && node.right !== null) {
 
     } else {
-
+      if (node.left) {
+        parent.left === node ? parent.left = node.left : parent.right = node.left;
+      } else {
+        parent.right === node ? parent.right = node.right : parent.left = node.right;
+      }
     }
   }
 }
 
 
-var binarySearchTree = new BinarySearchTree();
-binarySearchTree.insert(15).insert(20).insert(10).insert(12).insert(1).insert(5).insert(50);
+// var binarySearchTree = new BinarySearchTree();
+// binarySearchTree.insert(15).insert(20).insert(10).insert(12).insert(1).insert(5).insert(50);
+// binarySearchTree.remove(50);
+// console.log(binarySearchTree.root.right.value); // 20
+// console.log(binarySearchTree.root.right.right); // null
+// binarySearchTree.remove(5);
+// console.log(binarySearchTree.root.left.left.value); // 1
+// console.log(binarySearchTree.root.left.left.right); // null
 
-
-binarySearchTree.remove(50);
-console.log(binarySearchTree.root.right.value); // 20
-console.log(binarySearchTree.root.right.right); // null
-binarySearchTree.remove(5);
-console.log(binarySearchTree.root.left.left.value); // 1
+var binarySearchTree = new BinarySearchTree();binarySearchTree.insert(15).insert(20).insert(10).insert(12).insert(1).insert(5).insert(50);
+binarySearchTree.remove(1);
+console.log(binarySearchTree.root.left.left.value); // 5
+console.log(binarySearchTree.root.left.left.left); // null
 console.log(binarySearchTree.root.left.left.right); // null
+binarySearchTree.remove(20);
+console.log(binarySearchTree.root.right.value); // 50
+console.log(binarySearchTree.root.right.right); // null
+console.log(binarySearchTree.root.right.left); // null
 
-//var binarySearchTree = new BinarySearchTree();binarySearchTree .insert(15) .insert(20) .insert(10) .insert(12) .insert(1) .insert(5) .insert(50);binarySearchTree.remove(1);binarySearchTree.root.left.left.value // 5binarySearchTree.root.left.left.left // nullbinarySearchTree.root.left.left.right // nullbinarySearchTree.remove(20);binarySearchTree.root.right.value // 50binarySearchTree.root.right.right // nullbinarySearchTree.root.right.left // nullvar binarySearchTree = new BinarySearchTree();binarySearchTree .insert(15) .insert(20) .insert(10) .insert(12) .insert(1) .insert(5) .insert(50) .insert(60) .insert(30) .insert(25) .insert(23) .insert(24) .insert(70);binarySearchTree.remove(10);binarySearchTree.root.left.value // 12binarySearchTree.root.left.left.value // 1binarySearchTree.root.left.left.right.value // 5binarySearchTree.remove(50);binarySearchTree.root.right.value // 20binarySearchTree.root.right.right.value // 60binarySearchTree.root.right.right.left.value // 30var binarySearchTree = new BinarySearchTree();binarySearchTree .insert(22) .insert(49) .insert(85) .insert(66) .insert(95) .insert(90) .insert(100) .insert(88) .insert(93) .insert(89)binarySearchTree.remove(85);binarySearchTree.root.right.right.value // 88binarySearchTree.root.right.right.right.left.left.value // 89
+//var binarySearchTree = new BinarySearchTree();binarySearchTree .insert(15) .insert(20) .insert(10) .insert(12) .insert(1) .insert(5) .insert(50) .insert(60) .insert(30) .insert(25) .insert(23) .insert(24) .insert(70);binarySearchTree.remove(10);binarySearchTree.root.left.value // 12binarySearchTree.root.left.left.value // 1binarySearchTree.root.left.left.right.value // 5binarySearchTree.remove(50);binarySearchTree.root.right.value // 20binarySearchTree.root.right.right.value // 60binarySearchTree.root.right.right.left.value // 30var binarySearchTree = new BinarySearchTree();binarySearchTree .insert(22) .insert(49) .insert(85) .insert(66) .insert(95) .insert(90) .insert(100) .insert(88) .insert(93) .insert(89)binarySearchTree.remove(85);binarySearchTree.root.right.right.value // 88binarySearchTree.root.right.right.right.left.left.value // 89
