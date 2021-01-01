8dc6b22d60d9d9b12369fd2e77352c5372566d7e
Sy Rashid <sy.rashid.11@gmail.com>
2020-12-31 20:27:35 -0500

    FEAT: Set up test cases for count zero

--- section-5/count_zeros.js
+++ section-5/count_zeros.js
@@ -4,17 +4,17 @@ console.log(3, countZeroes([0,0,0])); // 3
 console.log(0, countZeroes([1,1,1,1])); // 0
 
 function countZeroes(arr) {
-
+  return modBinarySearch(arr);
 }
 
-function binarySearch(arr, target) {
+function modBinarySearch(arr) {
   let left = 0;
   let right = arr.length - 1;
 
   while (left <= right) {
     const middle = Math.ceil((right + left)/2);
-    if (arr[middle] === target) return middle;
-    (arr[middle] > target) ? right = middle - 1 : left = middle + 1;
+    if (arr[middle] === 0 && arr[middle - 1] === 1) return middle;
+    (arr[middle] > 0) ? right = middle - 1 : left = middle + 1;
   }
   return -1;
 }
